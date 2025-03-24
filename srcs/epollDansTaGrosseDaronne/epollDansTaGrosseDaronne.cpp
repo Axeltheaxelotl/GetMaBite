58dfd06e90c909a2e3ff0724583baf704cd9d2f0
Axel Lanty <alanty@c3r6s2.42luxembourg.lu>
2025-03-24 09:13:38 +0100

    je c pas

--- srcs/epollDansTaGrosseDaronne/epollDansTaGrosseDaronne.cpp
+++ srcs/epollDansTaGrosseDaronne/epollDansTaGrosseDaronne.cpp
@@ -24,9 +24,6 @@ epollDansTaGrosseDaronne::~epollDansTaGrosseDarone()
 // nom du serveur, IP, port, etc ... 
 void epollDansTaGrosseDaronne::setupServers(std::vector<ServerConfig> servers)
 {
-    // Logger::LogMsg enregistre un message dans console
-    // it.getServerName().c_str() la methode getServerName revoie le nom du serveur sous forme
-    // de chaine de caracteres, c_str convertit std::string une chaine C-style pour l aff dans le log
     Logger::LogMsg(LIGHTMAGENTA, CONSOLE_OUTPUT, "OUIIIIiiiiii....");
     _servers = servers;
 
@@ -34,5 +31,39 @@ void epollDansTaGrosseDaronne::setupServers(std::vector<ServerConfig> servers)
     for (auto &it : _servers)
     {
         it.setupServer(); // chaque serveur
-        Logger::logMsg(LIGHTMAGENTA, CONSOLE_OUTPUT, "Server Created: %s", it.getServerName().c_str());    }
+        // Logger::LogMsg enregistre un message dans console
+        // it.getServerName().c_str() la methode getServerName revoie le nom du serveur sous forme
+        // de chaine de caracteres, c_str convertit std::string une chaine C-style pour l aff dans le log
+        Logger::logMsg(LIGHTMAGENTA, CONSOLE_OUTPUT, "Server Created: %s", it.getServerName().c_str());
+    
+        // Ajouter chaque serveur a epoll pour la surveillance de la lecture
+        epoll_event event;
+        event.events = EPOLLIN | EPOLLET; // pour surveiller la lecture non-bloquant
+        event.data.fd = it.getFd(); // descripteur de fichier du serveur
+        addToEpoll(it.getFd(), event);
+    }
+}
+
+// boucle principale du serveur, gerant les evenements avec epoll
+void epollDansTaGrosseDaronne::serverRun()
+{
+    while (true)
+    {
+        // att evenement epoll
+        int event_count = epoll_wait(e_poll_fd, events, 10, -1); // blocage en att d un evenement
+
+        if (event_count == -1)
+        {
+            Logger::logMsg(RED, CONSOLE_OUTPUT, "Epoll wait error: %s", strerror(errno));
+            exit(1);
+        }
+
+        // gestion des evenements
+        for (int i = 0; i < event_count; ++i)
+        {
+        
+        }
+
+        
+    }
 }
