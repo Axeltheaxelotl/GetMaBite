d952ddca66dddba9162069603d3ced6a9534b62f
Axel Lanty <axellanty14@gmail.com>
2025-05-21 15:48:37 +0200

    la mere a simon elle suce

--- src/config/Server.cpp
+++ src/config/Server.cpp
@@ -6,12 +6,20 @@
 /*   By: alanty <alanty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 08:34:52 by smasse            #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/05/22 17:44:37 by alanty           ###   ########.fr       */
+=======
+/*   Updated: 2025/05/21 15:46:18 by alanty           ###   ########.fr       */
+>>>>>>> d876d56 (la mere a simon elle suce)
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"Server.hpp"
 
+<<<<<<< HEAD
 Server::Server() : client_max_body_size(1024 * 1024) {} 
+=======
+Server::Server() : client_max_body_size(10) {}
+>>>>>>> d876d56 (la mere a simon elle suce)
 
 Server::~Server() {}
