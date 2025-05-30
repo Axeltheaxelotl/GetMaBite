9e5a14b10ef3a5dfb9567cbb56643b419294d818
Axel Lanty <axellanty14@gmail.com>
2025-05-30 12:10:38 +0200

    AH AH ja reparer le git

--- src/config/Server.cpp
+++ src/config/Server.cpp
@@ -6,20 +6,12 @@
 /*   By: alanty <alanty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 08:34:52 by smasse            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/05/22 17:44:37 by alanty           ###   ########.fr       */
-=======
-/*   Updated: 2025/05/21 15:46:18 by alanty           ###   ########.fr       */
->>>>>>> d876d56 (la mere a simon elle suce)
+/*   Updated: 2025/05/30 10:17:45 by alanty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"Server.hpp"
 
-<<<<<<< HEAD
 Server::Server() : client_max_body_size(1024 * 1024) {} 
-=======
-Server::Server() : client_max_body_size(10) {}
->>>>>>> d876d56 (la mere a simon elle suce)
 
 Server::~Server() {}
